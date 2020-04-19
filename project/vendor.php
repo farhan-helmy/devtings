47d44bdaef179273afaa4bef0848e7c0c118462a
farhan-helmy <59960562+farhan-helmy@users.noreply.github.com>
2020-04-19 21:38:37 +0800

    project1

---
+++ project/vendor.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<?php
+	$name=$_POST["name"];
+	$email=$_POST["email"];
+	$number=$_POST["number"];
+	
+	echo "your name is $name";
+	echo "your number is $number";
+	echo "your email address is $email";
+	
+?>
+
+</body>
+</html>
